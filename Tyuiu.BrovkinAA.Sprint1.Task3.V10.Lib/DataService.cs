be064eb9e2d531a3091c58bd7cd6d8f285916c47
BrovkinAlex <Screep_dogg@mail.ru>
2024-09-22 12:11:46 +0300

    Task3

---
+++ Tyuiu.BrovkinAA.Sprint1.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.BrovkinAA.Sprint1.Task3.V10.Lib
+{
+    public class DataService : ISprint1Task3V10
+    {
+        public double NumberToMoney(double number)
+        {
+            if (Math.Round(number % Math.Floor(number), 2) != 1) 
+                return Math.Round(number % Math.Floor(number), 2) * 100;
+
+            return 0;
+        }
+    }
+}
---
+++ Tyuiu.BrovkinAA.Sprint1.Task3.V10/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BrovkinAA.Sprint1.Task3.V10.Lib;
+namespace Tyuiu.BrovkinAA.Sprint1.Task3.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт 1 | Выполнит Бровкин А. А. | ИБКСб-24-1";
+
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("*                                                                             *");
+            Console.WriteLine("* Спринт #1                                                                   *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                                 *");
+            Console.WriteLine("* Задание #3                                                                  *");
+            Console.WriteLine("* Вариант #10                                                                 *");
+            Console.WriteLine("* Выполнил Бровкин Алексей Александрович | ИБКСб-24-1                         *");
+            Console.WriteLine("*                                                                             *");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("*                                                                             *");
+            Console.WriteLine("* УСЛОВИЕ:                                                                    *");
+            Console.WriteLine("* Написать программу, которая преобразует введенное с клавиатуры              *");
+            Console.WriteLine("* дробное число в денежный формат и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                             *");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                            *");
+            Console.WriteLine("*******************************************************************************");
+
+            double x;
+            Console.WriteLine("\nВведите число: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n*******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                  *");
+            Console.WriteLine("*******************************************************************************\n");
+
+            Console.WriteLine(Math.Round(x, 3) + " руб. - это " + Math.Round(x) + " руб. " + ds.NumberToMoney(Math.Round(x, 3)) + " коп.");
+            Console.ReadKey();
+        }
+    }
+}
